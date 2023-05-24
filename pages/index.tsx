d72a5e7f3fd1fc505c533893e0b9e4e0f2f3929e
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-05-24 14:19:58 -0500

    Cleaning code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 
 export default function Home() {
   return (
@@ -19,13 +16,9 @@ export default function Home() {
         }}
       >
         <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example in TypeScript
+          Tinto
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+        
       </Box>
     </Container>
   );
