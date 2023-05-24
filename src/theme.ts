8b6a0a466e4aac1fd4c7b93e62488c1a787db269
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-05-24 15:04:41 -0500

    WIP theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { Roboto } from 'next/font/google';
+import { Roboto, Bree_Serif } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
@@ -9,22 +9,101 @@ export const roboto = Roboto({
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
 
+export const breeSerif = Bree_Serif({
+  weight: '400',
+  subsets: ['latin'],
+});
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
   typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
+        fontFamily: roboto.style.fontFamily,
+        h1: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        h2: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        h3: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        h4: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        h5: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        h6: {
+          fontFamily: breeSerif.style.fontFamily,
+        },
+        subtitle1: {
+          fontFamily: 'Droid Serif',
+        },
+        subtitle2: {
+          fontFamily: 'Droid Serif',
+        },
+      },
+      palette: {
+        primary: {
+          main: '#080911',
+          contrastText: '#ffffff',
+        },
+        secondary: {
+          main: '#f4212e',
+        },
+        background: {
+          default: '#dde2ef',
+          paper: '#ffffff',
+        },
+      },
+
 });
 
 export default theme;
+
+
+
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+// export const themeOptions: ThemeOptions = {
+//   typography: {
+//     fontFamily: 'Roboto',
+//     h1: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     h2: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     h3: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     h4: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     h5: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     h6: {
+//       fontFamily: 'Bree Serif',
+//     },
+//     subtitle1: {
+//       fontFamily: 'Droid Serif',
+//     },
+//     subtitle2: {
+//       fontFamily: 'Droid Serif',
+//     },
+//   },
+//   palette: {
+//     primary: {
+//       main: '#080911',
+//       contrastText: '#ffffff',
+//     },
+//     secondary: {
+//       main: '#f4212e',
+//     },
+//     background: {
+//       default: '#dde2ef',
+//       paper: '#ffffff',
+//     },
+//   },
+// };
+
