2f13e333d29d40c69336dc932324113047c128fc
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-05-29 19:24:40 -0500

    WIP adding autocomplete for wines

--- pages/app/likes.tsx
+++ pages/app/likes.tsx
@@ -1,20 +1,69 @@
-import { AuthContext } from "@/context/auth";
-import { Favorite, HeartBroken, Home, Menu, StyleRounded } from "@mui/icons-material";
-import { AppBar, BottomNavigation, BottomNavigationAction, Box, Button, Container, Fab, IconButton, Paper, Toolbar, Typography } from "@mui/material";
-import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import { NextLinkComposed } from "@/Link";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { AddCircleOutlineOutlined, PlusOneRounded } from "@mui/icons-material";
+import { Autocomplete, Box, Container, IconButton, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+import { useQuery } from 'urql';
+
+
 export default function Likes() {
+
+    const [wineName, setWineName] = useState('');
+    const [wineSelected, setWineSelected] = useState<any>({});
+    const [wineOptions, setWineOptions] = useState<[]>([]);
+    const [result] = useQuery({
+        query: `query MyQ($wine: String) {
+            allReds(filter: {wine: $wine}, perPage:10, page: 0) {
+              wine,
+              image,
+              id,
+          
+            }
+          }`,
+        pause: wineName === '' || wineName.length < 3,
+        variables: {
+            wine: wineName,
+        }
+    });
+
     return <>
         <Container maxWidth="lg">
             <Header />
-            <h1>Likes</h1>
+            <Box
+                sx={{
+                    my: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    //   alignItems: 'center',
+                }}
+            >
+                <Typography variant="h4" component="p" gutterBottom>
+                    Wines you liked
+                    <IconButton>
+                        <AddCircleOutlineOutlined />
+                    </IconButton>
+                </Typography>
+                {/* <TextField value={wineName} onChange={(evt) => setWineName(evt.target.value)} /> */}
+                <Autocomplete
+                    freeSolo
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    value={wineName}
+                    options={[{
+                        value: 'A', label: 'A',
+                    }]}
+                    renderInput={(params) => (
+                        <TextField {...params} label={'Wine name'} fullWidth />
+                    )}
+                    filterOptions={(x) => x}
+                    onInputChange={(evt, newInputVal) => {
+                        setWineName(newInputVal);
+                    }}
+                />
+            </Box>
             <Footer />
         </Container>
     </>
